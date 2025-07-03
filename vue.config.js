a97eef63cfda27748d400ea2210528276b0fb2da
Ron Appleton <ron.appleton@gmail.com>
2025-07-03 21:23:18 +0100

    Make secure for testing

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+const fs = require('fs')
+const path = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+        https: {
+            key: fs.readFileSync(path.join(__dirname, 'certs/key.pem')),
+            cert: fs.readFileSync(path.join(__dirname, 'certs/cert.pem')),
+        },
+        port: 3000,
+        host: '0.0.0.0',
+    }
 })
